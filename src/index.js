44f797dfbeae5c9a71de64019b48628c47bd862e
Dmitry Bogodyazh <dima3911784@hotmail.com>
2020-02-20 23:26:39 +0300

    feat: implement function towelSort()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    const result = [];
+    if (!matrix) return [];
+    matrix.map(el => {
+        if (matrix.indexOf(el) % 2 === 0) {
+            el.map(el => result.push(el));
+        } else {
+            el.reverse().map(el => result.push(el));
+        }
+    });
+    return result;
+};
